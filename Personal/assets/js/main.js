076716d9c9a79408adb9f4e06a3d95a9039018b1
Shirazi03 <112660284+Shirazi03@users.noreply.github.com>
2022-11-07 02:19:53 +0530

    Update main.js
    Signed-off-by: Shirazi03 <112660284+Shirazi03@users.noreply.github.com>

--- Personal/assets/js/main.js
+++ Personal/assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Personal - v4.8.1
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
